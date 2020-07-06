6d9008f14fc970839ad2a674dfd9fbf9ee0bfe05
haixiang <373971510@qq.com>
2020-07-06 20:48:54 +0800

    分页功能实现

--- src/pages/flight/index.tsx
+++ src/pages/flight/index.tsx
@@ -1,24 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import { Table, Radio, Divider } from 'antd';
+import { Table, Radio,Tag, Divider } from 'antd';
 import { flightList } from '../../api/flight'
-const columns = [
-  {
-    title: '机票ID',
-    dataIndex: 'air_ticket_id',
-  },
-  {
-    title: '出发机场',
-    dataIndex: 'depAirport',
-  },
-  {
-    title: '到达机场',
-    dataIndex: 'arrAirport',
-  },
-];
 
 
 const rowSelection = {
-  
   onChange: (selectedRowKeys:any, selectedRows:any) => {
     console.log(`selectedRowKeys: ${selectedRowKeys}`, 'selectedRows: ', selectedRows);
   },
@@ -28,8 +13,12 @@ const rowSelection = {
   }),
 };
 
+//点击编辑或删除
+
 const Flight:React.FC = () => {
+     
   const [data,setData] = useState([])
+  const [total,setTotal] = useState(0)
   const [selectionType, setSelectionType] = useState('checkbox');
   const [model,setModel] = useState({
       page:1,
@@ -40,8 +29,55 @@ const Flight:React.FC = () => {
       depdate:'',
       aircode:''
   })
-
+  const [loading,setLoading] = useState(false)
+  const columns = [
+    {
+      title: '机票ID',
+      dataIndex: 'air_ticket_id',
+    },
+    
+    {
+      title: '出发机场',
+      dataIndex: 'depAirport',
+    },
+    {
+      title: '到达机场',
+      dataIndex: 'arrAirport',
+    },
+    {
+        title:'出发日期',
+        dataIndex:'depDate',
+    },
+    {
+        title:'出发时间',
+        dataIndex:'depTime'
+    },
+    {
+        title:'到达时间',
+        dataIndex:'arrTime'
+    },
+    {
+        title:'飞行耗时',
+        dataIndex:'flightTime'
+    },
+    {
+      title: 'Action',
+      key: 'action',
+      render: (text:any, record:any) => (
+            <span>
+            <a style={{ marginRight: 16 }} onClick={() => handleitem(record,'editor')}>编辑</a>
+            <a onClick={() => handleitem(record,'delete')}>删除</a>
+            </span>
+        ),
+      },
+  ];
+  function onChangepage(page:number,pagesize:number | undefined) {
+    model.page = page
+    setModel(model)
+    getFlightInfo()
+  }
   function getFlightInfo() { 
+    setLoading(true);
     flightList(model).then(res => {
         let result
         result =  res.data.map((item:any) => {
@@ -49,6 +85,10 @@ const Flight:React.FC = () => {
             return item
         })
         setData(result)
+        setTotal(res.len)
+        setTimeout(() => {
+            setLoading(false);
+        },500)
     })
   }
   useEffect(() => {
@@ -58,7 +98,7 @@ const Flight:React.FC = () => {
     <div>
       <Radio.Group
         onChange={({ target: { value } }) => {
-          setSelectionType(value);
+          setSelectionType(value);          
         }}
         value={selectionType}
       >
@@ -70,14 +110,26 @@ const Flight:React.FC = () => {
 
       <Table
         rowSelection={{
-            type: 'checkbox',
+            type: selectionType,
             ...rowSelection,
         }}
         columns={columns}
         dataSource={data}
-      />
+        pagination={
+            {
+                total,
+                defaultPageSize:model.limit,
+                onChange:(page,pagesize) => onChangepage(page,pagesize)
+            }
+        }
+        loading={loading}
+      >
+      </Table>
     </div>
   );
+  function handleitem(record:any,state:string) {
+    
+    }   
 };
 
 export default Flight
---
+++ src/pages/flight/tableOption.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+
+const options = () => {
+    
+}
+export default options
